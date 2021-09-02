67fc92fdc0c0bfd3cff3b529ca053daf017c0903
Aleksandrina <89221540582@mail.ru>
2021-09-02 22:12:47 +0500

    добавлено задание 4

--- task_4/solution.js
+++ task_4/solution.js
@@ -6,7 +6,7 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-    let data = json.parse(json);
+    let data = JSON.parse(json);
     let products = data.products;
     return products;
 }
